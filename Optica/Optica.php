4420064856a6358f93d3a45bc2653ab9804a882d
Abinash <123747322+AbiXnash@users.noreply.github.com>
2023-04-09 22:37:45 +0530

    Add files via upload

---
+++ Optica/Optica.php
@@ -0,0 +1,142 @@
+<?php
+   include('../hf/clustersheader.html');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Optica 2k23</title>
+      <link rel="icon" type="image/png" href="images/elogo.png">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+      <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+      <link rel="stylesheet" href="css/style.css">
+   </head>
+   <body>
+      <section class="heading-link">
+         <h3>Optica Registration</h3>
+         <p><a href="../index.php"> Theta </a> <a href="../clusters.php"> / Clusters </a> / Optica </p>
+      </section>
+      <section class="about" style="text-align: center;">
+			<div class="content">
+				<h3 class="about-title" style="font-size: 3.6rem;">About Optica</h3>
+				<p style="font-size: 1.7rem;">blah blah blah<br></p>
+            <p style="font-size: 2.1rem; margin-top: -20px;">
+               <big>
+                  <b style="font-weight: 1200; padding-left: 30%; padding-right: 30%;">
+                     "Things Just Aren't That Simple"
+                  </b>
+               </big>
+            </p>
+			</div>
+		</section>
+      <section class="contest">
+         <h1 class="heading"> Contest Registration </h1>
+         <h2 class="heading">Coming Soon...</h2>
+         <div class="icons-container">
+            <div class="contest-container">
+               <div class="contest-box">
+                  <i class='bx bx-game'></i>
+                  <h3>Treasure Hunt</h3>
+                  <p>Last Date for Registration :<br>  - 04 - 2023</p>
+                  <a href = "#" class = "btn">Register Now</a>
+               </div>
+               <div class="contest-box">
+                  <i class='bx bx-game'></i>
+                  <h3>Jodo Jeetho</h3>
+                  <p>Last Date for Registration :<br> 20 - 04 - 2023</p>
+                  <a href = "#" class = "btn">Register Now</a>
+               </div>
+               <div class="contest-box">
+                  <i class='bx bx-game'></i>
+                  <h3>Kick the Luck</h3>
+                  <p>Last Date for Registration :<br> 20 - 04 - 2023</p>
+                  <a href = "#" class = "btn">Register Now</a>
+               </div>
+               <div class="contest-box">
+                  <i class='bx bx-game'></i>
+                  <h3>SOS</h3>
+                  <p>Last Date for Registration :<br> 20 - 04 - 2023</p>
+                  <a href = "#" class = "btn">Register Now</a>
+               </div>
+            </div>
+         </div>
+      </section>
+      <section class="faq">
+         <h1 class="heading">About our Events</h1>
+         <div class="accordion-container">
+            <div class="accordion active">
+               <div class="accordion-heading">
+                  <h3>Treasure Hunt?</h3>
+                  <i class="fas fa-angle-down"></i>
+               </div>
+               <p class="accordion-content">
+                  Currently enrolled college students can register for a short film contest with a PG-13 theme, with a 7-8 minute limit on the film's duration. The registration concludes on April 20th, 2023 and the deadline for submission is April 25th, 2023. Cash prizes of Rs. 15,000, Rs. 7,000, and Rs. 3,000 will be awarded to the top three films.
+               </p>
+            </div>
+            <div class="accordion">
+               <div class="accordion-heading">
+                  <h3>Jodo Jeetho?</h3>
+                  <i class="fas fa-angle-down"></i>
+               </div>
+               <p class="accordion-content">
+                  Currently enrolled college students can register for a AD film contest, with a 2-3 minute limit on the AD's duration. The registration concludes on April 20th, 2023 and the deadline for submission is April 25th, 2023. Cash prizes of Rs. 3,000, Rs. 1,500, and Rs. 500 will be awarded to the top three films.
+               </p>
+            </div>
+            <div class="accordion">
+               <div class="accordion-heading">
+                  <h3>Kick the Luck?</h3>
+                  <i class="fas fa-angle-down"></i>
+               </div>
+               <p class="accordion-content">
+                  Currently enrolled college students can register for a MashUp contest with a theme of biography or creating awareness for society, with a duration of 2-3 minutes and may contain copyrighted material. Submissions online and prizes of Rs. 3000, Rs. 1500, and Rs. 500 for the top 3 winners.
+            </div>
+            <div class="accordion">
+               <div class="accordion-heading">
+                  <h3>SOS?</h3>
+                  <i class="fas fa-angle-down"></i>
+               </div>
+               <p class="accordion-content">
+                  This photography contest is open to college students and has two themes: digital camera photography (reflection) and mobile photography (environmental portrait). Registration concludes on April 15th, with the submission deadline on April 25th. Prizes are awarded for both categories: Rs. 3,000, Rs. 1,500, and Rs. 500 for digital camera photography, and Rs. 1,500, Rs. 700, and Rs. 300 for mobile photography. Participants must not upload photos on any social media platforms and follow all event guidelines.
+               </p>
+            </div>
+         </div>
+      </section>
+      <section class="contact">
+         <h1 class="heading"> Contact Us </h1>
+         <div class="row">
+            <div class="image">
+               <img src="images/contact-img.png" alt="">
+            </div>
+            <div class="icons-container">
+               <div class="icons">
+                  <i class="fas fa-phone"></i>
+                  <h3>Phone :</h3>
+                  <label style="font-size: medium; color: #868686;">Surya   : </label><a href="#" style="font-size: medium;">+91 93456 42528</a><br>
+                  <label style="font-size: medium; color: #868686;">Ranil   : </label><a href="#" style="font-size: medium;">+91 93456 42528</a><br>
+                  <label style="font-size: medium; color: #868686;">Abinash : </label><a href="#" style="font-size: medium;">+91 93456 42528</a>
+               </div>
+               <div class="icons">
+                  <i class="fas fa-envelope"></i>
+                  <h3>Email : </h3>
+                  <a href="#" style="font-size: medium;">theta.emulsion@gmail.com</a>
+               </div>
+               <div class="icons">
+                  <i class="fas fa-map"></i>
+                  <h3>Instagram :</h3>
+                  <a href="#" style="font-size: medium;">team_emulsion</a>
+               </div>
+            </div>
+         </div>
+      </section>
+      <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+      <script src="js/script.js"></script>
+      <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+   </body>
+</html>
+
+<?php
+   include('../hf/clustersfooter.html');
+?>
