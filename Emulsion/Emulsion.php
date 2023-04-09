dd09d5bf1b5f0b37b3c47e24984d99fd7ddc71fa
Abinash <123747322+AbiXnash@users.noreply.github.com>
2023-04-09 22:34:45 +0530

    Add files via upload

---
+++ Emulsion/Emulsion.php
@@ -0,0 +1,167 @@
+<?php
+   include('../hf/clustersheader.html');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Emulsion 2k23</title>
+      <link rel="icon" type="image/png" href="images/elogo.png">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+      <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+      <link rel="stylesheet" href="css/style.css">
+   </head>
+   <body>
+      <section class="heading-link">
+         <h3>Emulsion Registration</h3>
+         <p><a href="../index.php"> Theta </a> <a href="../clusters.php"> / Clusters </a> / Emulsion </p>
+      </section>
+      <section class="about" style="text-align: center;">
+			<div class="content">
+				<h3 class="about-title" style="font-size: 3.6rem;">About Emulsion</h3>
+				<p style="font-size: 1.7rem;">Emulsion celebrates creativity and innovation in various forms of media and art. It includes contests such as photography, videography, AD, and short films. Participants showcase their skills and creativity in friendly competition.<br></p>
+            <p style="font-size: 2.1rem; margin-top: -20px;">
+               <big>
+                     "We Speak Cinema"
+               </big>
+            </p>
+			</div>
+		</section>
+      <section class="contest">
+         <h1 class="heading"> Contest Registration </h1>
+         <div class="icons-container">
+            <div class="contest-container">
+               <div class="contest-box">
+                  <i class='bx bx-game'></i>
+                  <h3>Short Film Contest Registration</h3>
+                  <p>Last Date for Registration :<br> 20 - 04 - 2023</p>
+                  <p style="margin-top: -10px; color: #865686;"><b>Submission dates: 20th April - 25 April</b></p>
+                  <a target="_blank" href = "https://forms.gle/rxBjGGZVu19c7BRD6" class = "btn">Register Now</a>
+               </div>
+               <div class="contest-box">
+                  <i class='bx bx-game'></i>
+                  <h3>Mobile Photography Contest</h3>
+                  <p>Last Date for Registration :<br> 20 - 04 - 2023</p>
+                  <p style="margin-top: -10px; color: #865686;"><b>Submission dates: 20th April - 25 April</b></p>
+                  <a target="_blank" href = "https://forms.gle/8GNo9nXZGF6m21dp9" class = "btn">Register Now</a>
+               </div>
+               <div class="contest-box">
+                  <i class='bx bx-game'></i>
+                  <h3>AD Film Contest</h3>
+                  <p>Last Date for Registration :<br> 20 - 04 - 2023</p>
+                  <p style="margin-top: -10px; color: #865686;"><b>Submission dates: 20th April - 25 April</b></p>
+                  <a target="_blank" href = "https://forms.gle/F7NPjFgcvQzMG8aq5" class = "btn">Register Now</a>
+               </div>
+               <div class="contest-box">
+                  <i class='bx bx-game'></i>
+                  <h3>MashUp Contest</h3>
+                  <p>Last Date for Registration :<br> 20 - 04 - 2023</p>
+                  <p style="margin-top: -10px; color: #865686;"><b>Submission dates: 20th April - 25 April</b></p>
+                  <a target="_blank" href = "https://forms.gle/kR8bNsmTP3xvD3S18" class = "btn">Register Now</a>
+               </div>
+               <div class="contest-box">
+                  <i class='bx bx-game'></i>
+                  <h3>Digital Camera Photography Contest</h3>
+                  <p>Last Date for Registration :<br> 20 - 04 - 2023</p>
+                  <p style="margin-top: -10px; color: #865686;"><b>Submission dates: 20th April - 25 April</b></p>
+                  <a target="_blank" href = "https://forms.gle/8GNo9nXZGF6m21dp9" class = "btn">Register Now</a>
+               </div>
+               <div class="contest-box">
+                  <i class='bx bx-game'></i>
+                  <h3>On spot Photography Contest</h3>
+                  <p>Last Date for Registration :<br> 20 - 04 - 2023</p>
+                  <p style="margin-top: -10px; color: #865686;"><b>Submission dates: 20th April - 25 April</b></p>
+                  <a target="_blank" href = "https://forms.gle/hTkmpgyJzKYuSpiL7" class = "btn">Register Now</a>
+               </div>
+            </div>
+         </div>
+      </section>
+      <section class="faq">
+         <h1 class="heading">About our Contest</h1>
+         <div class="accordion-container">
+            <div class="accordion active">
+               <div class="accordion-heading">
+                  <h3>Short Film Contest?</h3>
+                  <i class="fas fa-angle-down"></i>
+               </div>
+               <p class="accordion-content">
+                  Currently enrolled college students can register for a short film contest with a PG-13 theme, with a 7-8 minute limit on the film's duration. The registration concludes on April 20th, 2023 and the deadline for submission is April 25th, 2023. Cash prizes of Rs. 15,000, Rs. 7,000, and Rs. 3,000 will be awarded to the top three films.
+               </p>
+            </div>
+            <div class="accordion">
+               <div class="accordion-heading">
+                  <h3>AD Film Contest?</h3>
+                  <i class="fas fa-angle-down"></i>
+               </div>
+               <p class="accordion-content">
+                  Currently enrolled college students can register for a AD film contest, with a 2-3 minute limit on the AD's duration. The registration concludes on April 20th, 2023 and the deadline for submission is April 25th, 2023. Cash prizes of Rs. 3,000, Rs. 1,500, and Rs. 500 will be awarded to the top three films.
+               </p>
+            </div>
+            <div class="accordion">
+               <div class="accordion-heading">
+                  <h3>MashUp Contest?</h3>
+                  <i class="fas fa-angle-down"></i>
+               </div>
+               <p class="accordion-content">
+                  Currently enrolled college students can register for a MashUp contest with a theme of biography or creating awareness for society, with a duration of 2-3 minutes and may contain copyrighted material. Submissions online and prizes of Rs. 3000, Rs. 1500, and Rs. 500 for the top 3 winners.
+            </div>
+            <div class="accordion">
+               <div class="accordion-heading">
+                  <h3>Photography Contest?</h3>
+                  <i class="fas fa-angle-down"></i>
+               </div>
+               <p class="accordion-content">
+                  This photography contest is open to college students and has two themes: digital camera photography (reflection) and mobile photography (environmental portrait). Registration concludes on April 15th, with the submission deadline on April 25th. Prizes are awarded for both categories: Rs. 3,000, Rs. 1,500, and Rs. 500 for digital camera photography, and Rs. 1,500, Rs. 700, and Rs. 300 for mobile photography. Participants must not upload photos on any social media platforms and follow all event guidelines.
+               </p>
+            </div>
+            <div class="accordion">
+               <div class="accordion-heading">
+                  <h3>On spot Photography Contest?</h3>
+                  <i class="fas fa-angle-down"></i>
+               </div>
+               <p class="accordion-content">
+                  On Spot Photography Contest for for currently enrolled college students. Theme will be revealed during the fest. Participants have 30 minutes to capture their best shot on the day of the event. Registration fee is Rs. 50 and prizes are Rs. 1,500 for first place, Rs. 700 for second place, and Rs. 300 for third place.
+               </p>
+            </div>
+         </div>
+      </section>
+      <section class="contact">
+         <h1 class="heading"> Contact Us </h1>
+         <div class="row">
+            <div class="image">
+               <img src="images/contact-img.png" alt="">
+            </div>
+            <div class="icons-container">
+               <div class="icons">
+                  <i class="fas fa-phone"></i>
+                  <h3>Phone :</h3>
+                  <label style="font-size: medium; color: #868686;">Surya   : </label><a style="font-size:medium;" href="tel:+919360617308" onclick="event.preventDefault(); window.location.href='tel:+919360617308';">+91 93606 17308</a><br>
+                  <label style="font-size: medium; color: #868686;">Ranil   : </label><a style="font-size:medium;" href="tel:+919994679774" onclick="event.preventDefault(); window.location.href='tel:+919994679774';">+91 99946 79774</a><br>
+                  <label style="font-size: medium; color: #868686;">Abinash : </label><a style="font-size:medium;" href="tel:+919345642528" onclick="event.preventDefault(); window.location.href='tel:+919345642528';">+91 93456 42528</a>
+
+               </div>
+               <div class="icons">
+                  <i class="fas fa-envelope"></i>
+                  <h3> Email : </h3>
+                  <a href="mailto:theta.emulsion@gmail.com" target="_blank" style="font-size: medium;">theta.emulsion@gmail.com</a>
+               </div>
+               <div class="icons">
+                  <i class="fas fa-map"></i>
+                  <h3>Instagram :</h3>
+                  <a href="https://instagram.com/team_emulsion?igshid=MGNiNDI5ZTU=" target="_blank" style="font-size: medium;">team_emulsion</a>
+               </div>
+            </div>
+         </div>
+      </section>
+      <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+      <script src="js/script.js"></script>
+      <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+   </body>
+</html>
+
+<?php
+   include('../hf/clustersfooter.html');
+?>
